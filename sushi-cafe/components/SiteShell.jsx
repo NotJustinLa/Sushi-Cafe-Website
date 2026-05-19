186b090eb351356f02a4b14d1c23c8813d39d6f2
NotJustinLa <jlaa0008@student.monash.edu>
2026-05-19 21:39:51 +1000

    feat(backend): Created siteshell file responsible for displaying and removing the splash loading page

---
+++ sushi-cafe/components/SiteShell.jsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
+import LoadingSplash from "./LoadingSplash";
+
+const MIN_DURATION = 1700 //that the splash shows for
+
+export default function SiteShell({ children}) {
+    // loading starts as True so the splash is visible
+    // when it turns to false, the splash animates out
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // record the exact moment this component mounted
+        // mounted refers to first initalised on visual structure
+        const start = performance.now()
+
+        // This function is called when the page is finished loading
+        // It ensures that it runs the rest oif the remaining time
+        // setTimeout runs code after a delay
+        // clearTimeout clears the timer.
+        function dismiss() {
+            const elapsed = performance.now()-start; //how long has it been
+            const remaining = Math.max(0, MIN_DURATION -elapsed) // how long to wait
+            const t = setTimeout(() => setLoading(false), remaining) // hide the splash after the remaining time
+            return () => clearTimeout(t) //cancel the timeout so it doesn’t keep running.”
+    }
+
+    // if the webpage is loaded, return the dismiss function above
+    if (document.readyState === "complete") {
+        return dismiss();
+    }
+
+    let cleanupDismiss;
+    const onLoad = () => {
+        cleanupDismiss = dismiss()
+    };
+    window.addEventListener("load", onLoad);
+    return () => {
+        window.removeEventListener("load", onLoad);
+        if (cleanupDismiss) cleanupDismiss();
+    };
+
+}, []);
+
+return (
+    <>
+        <AnimatePresence>
+            {loading && <LoadingSplash key="splash" />}
+        </AnimatePresence>
+        {children}
+    </>
+    );
+}
